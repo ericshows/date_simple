a8a80dddf151a082ed9d664e386ba5b6afda8029
Eric Shows <ericshows@nypl.org>
2018-04-16 02:22:39 -0400

    adding files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup( name='date_simple',
+       version='0.1',
+       description='This module greets the user.  ',
+       url='https://github.com/ericshows/date_simple',                # usually a github URL
+       author='EricShows    ',
+       author_email='erictshows@gmail.com',
+       license='MIT',
+       packages=['date_simple'],
+       install_requires=[ ],
+       test_suite='pytest',
+       tests_require=['pytest'],
+       zip_safe=False )
